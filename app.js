7646b1b674e4b67beaabc19b5e4114c5406264f7
khalidev21 <khalidev21@gmail.com>
2021-07-13 21:17:51 +0200

    Mi primer commit

---
+++ app.js
@@ -0,0 +1,76 @@
+
+/*Menu*/ 
+var btnMenu= document.getElementById("btn-menu");
+var menu= document.getElementById("menu");
+
+btnMenu.addEventListener("click", ()=>{
+    menu.classList.toggle("efecto")
+    btnMenu.classList.toggle("fa-times");
+    
+    
+})
+/*modo dark*/
+var body= document.getElementById("body");
+var btnModo=document.getElementById("btn-modo");
+var etxe=document.getElementById("etxe");
+var coding= document.getElementById("coding");
+
+
+btnModo.addEventListener("click", ()=>{
+    body.classList.toggle("darkmode");
+    if(body.className==""){
+
+    
+    etxe.src ="fotos/etxe2.png";
+    coding.src ="fotos/coding.png";
+    }else{
+        etxe.src="fotos/etxe-dark2.png";
+        coding.src="fotos/coding-dark.png";
+    
+    }
+
+
+});
+
+/*tarjetas*/
+var biografia=document.getElementById("biografia");
+var biografiaA= document.getElementById("biografia-a");
+var habilidades=document.getElementById("habilidades");
+var habilidadesA= document.getElementById("habilidades-a");
+var aficiones=document.getElementById("aficiones");
+var aficionesA= document.getElementById("aficiones-a");
+
+biografiaA.addEventListener("click", ()=>{
+    biografia.style.zIndex="3";
+    biografia.classList.toggle("js-scale")
+    aficiones.style.zIndex="1";
+    habilidades.style.zIndex="2";
+    
+})
+
+
+habilidadesA.addEventListener("click", ()=>{
+    habilidades.style.zIndex="3";
+    habilidades.classList.toggle("js-scale");
+    biografia.style.zIndex="2";
+    aficiones.style.zIndex="1";
+   
+    
+});
+
+aficionesA.addEventListener("click", ()=>{
+    aficiones.style.zIndex="3";
+    aficiones.classList.toggle("js-scale")
+    biografia.style.zIndex="1";
+    habilidades.style.zIndex="2";
+    
+})
+
+
+
+
+
+
+
+
+
